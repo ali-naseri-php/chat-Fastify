fd4850ed59d87206b6d10e8335b43d3386c6aefd
09361819903ali <ali.naseri.php@gmail.com>
2025-03-02 17:20:38 +0000

    add config for mongodb

---
+++ src/config/config.mongoose.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import * as  dotenv from "dotenv"
+
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/testdb";
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log("MongoDB Connected!");
+    } catch (error) {
+        console.error(" MongoDB Connection Failed!", error);
+        process.exit(1);
+    }
+};
