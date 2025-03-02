130dd90b00c165e517ff74e0456033d03ce1542d
09361819903ali <ali.naseri.php@gmail.com>
2025-03-02 17:36:06 +0000

    add model for user

---
+++ src/modules/auth/schemas/user.schema.ts
@@ -0,0 +1,15 @@
+import { Schema, model } from "mongoose";
+
+export interface IUser {
+    username: string;
+    email: string;
+    password: string;
+}
+
+const UserSchema = new Schema<IUser>({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+});
+
+export const UserModel = model<IUser>("User", UserSchema);
