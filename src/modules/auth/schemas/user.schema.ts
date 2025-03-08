091be81e7dd45366ba69b064dc5d081126fbb839
09361819903ali <ali.naseri.php@gmail.com>
2025-03-08 15:07:00 +0000

    Fix: Separate interface from User model to improve code structure

--- src/modules/auth/schemas/user.schema.ts
+++ src/modules/auth/schemas/user.schema.ts
@@ -1,15 +1,10 @@
 import { Schema, model } from "mongoose";
-
-export interface IUser {
-    username: string;
-    email: string;
-    password: string;
-}
+import { IUser } from "../interfaces/user.interface";
 
 const UserSchema = new Schema<IUser>({
-    username: { type: String, required: true, },
+    username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-});
+}, { timestamps: true });
 
 export const UserModel = model<IUser>("User", UserSchema);
